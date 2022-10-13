0bb3557baf279a5ce35497ea401bd65c8163339c
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-10-13 16:24:59 -0700

    organize directory, scaffold base  components

---
+++ src/components/RestaurantCharts.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RestaurantCharts() {
+  return (
+    <div>
+      RestaurantCharts - Top Rated restaurants by area, cuisine style, and
+      overall
+    </div>
+  );
+}
+
+export default RestaurantCharts;
