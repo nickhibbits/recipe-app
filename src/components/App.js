62bd4b657e71d7227e2fd8dea57f7da87d4c0ce4
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-10-13 19:07:16 -0700

    match paths and component names, finish routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 
 import Dashboard from "./Dashboard";
 import RestaurantProfile from "./RestaurantProfile";
+import UserProfile from "./UserProfile";
+import RestaurantCharts from "./RestaurantCharts";
+import MyRestaurants from "./MyRestaurants";
 import "../styles/App.css";
 
 function App() {
@@ -9,7 +12,10 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" exact element={<Dashboard />} />
-        <Route path="/restaurant/" exact element={<RestaurantProfile />} />
+        <Route path="/restaurantProfile/:id" element={<RestaurantProfile />} />
+        <Route path="userProfile/:userId" element={<UserProfile />} />
+        <Route path="restaurantCharts" element={<RestaurantCharts />} />
+        <Route path="myRestaurants" element={<MyRestaurants />} />
       </Routes>
     </div>
   );
---
+++ src/components/MyRestaurants.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyRestaurants() {
+  return (
+    <div>
+      MyRestaurants - List of reviewed and saved restaurants specific to logged
+      in user
+    </div>
+  );
+}
+
+export default MyRestaurants;
