a236dca62c4abf25a362bf2bb562024b7da569fb
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-11-25 21:59:15 -0800

    scaffold MyRecipes component

--- client/src/components/app/MyRecipes.js
+++ client/src/components/app/MyRecipes.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import RecipeScrollContainer from "./RecipeScrollContainer";
 
-function MyRecipes() {
+function MyRecipes({ users, currentUser }) {
+  const myRecipes = users.byId[currentUser].savedRecipes;
+
   return (
-    <div>
-      MyRecipes - List of reviewed and saved Recipes specific to logged in user
-    </div>
+    <main className="my-recipes-component component">
+      <h1 className="page-heading">My Recipes</h1>
+      <RecipeScrollContainer
+        recipes={myRecipes}
+        categoryTitle={"Saved Recipes"}
+      />
+    </main>
   );
 }
 
-export default MyRecipes;
+const mapStateToProps = ({ users, auth }) => {
+  return {
+    users,
+    currentUser: auth.user,
+  };
+};
+
+export default connect(mapStateToProps)(MyRecipes);
