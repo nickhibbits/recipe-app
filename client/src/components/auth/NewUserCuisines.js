c7f21fea4cd6cab45e5fba0edaffbe669b4b951c
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-11-29 16:18:22 -0800

    develop newUserCuisines UI

--- client/src/components/auth/NewUserCuisines.js
+++ client/src/components/auth/NewUserCuisines.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import "../../styles/App.scss";
+
 function NewUserCuisines() {
   const allCategories = [
     "African",
@@ -37,20 +39,33 @@ function NewUserCuisines() {
     setCategoryList((currentList) => [...currentList, cuisine]);
   };
 
+  const handleSubmitNewUserCuisines = (cuisines) => {
+    console.log("cuisines", cuisines);
+  };
+
   return (
-    <ul className="cusine-select">
-      {allCategories.map((category, i) => {
-        return (
-          <li
-            className="category-list-item"
-            onClick={() => addUserCuisine(category)}
-            key={i}
-          >
-            {category}
-          </li>
-        );
-      })}
-    </ul>
+    <main className="new-user-cuisines-component component">
+      <h1 className="title">Select Cuisines</h1>
+      <ul className="cusine-select">
+        {allCategories.map((category, i) => {
+          return (
+            <li
+              className="category-list-item"
+              onClick={() => addUserCuisine(category)}
+              key={i}
+            >
+              {category}
+            </li>
+          );
+        })}
+      </ul>
+      <button
+        onClick={() => handleSubmitNewUserCuisines(userCategories)}
+        className="submit-cuisines"
+      >
+        Submit
+      </button>
+    </main>
   );
 }
 
